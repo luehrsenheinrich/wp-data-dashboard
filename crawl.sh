5a79b557093c20d28f446c5cd7c67ffe6f464d9c
Hendrik Luehrsen <hendrik@luehrsen.de>
2023-10-14 21:27:33 +0200

    Fix working dir

--- crawl.sh
+++ crawl.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 # This script starts the crawl process
 
+cd ~/symfony
 symfony console app:crawl:themes
--- startWorkers.sh
+++ startWorkers.sh
@@ -2,5 +2,6 @@
 # A script that stops all workers and restarts them
 # This bash script is called by a cron job every 5 minutes
 
+cd ~/symfony
 symfony console messenger:stop-workers
 symfony console messenger:consume async -vv --time-limit=295 --memory-limit=256M
