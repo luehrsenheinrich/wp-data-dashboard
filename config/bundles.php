e89d04081dcd7c7df70ae813fb88d98bfc3e44e0
Hendrik Luehrsen <hendrik@luehrsen.de>
2023-11-10 21:31:16 +0100

    Resolve cors issues

--- config/bundles.php
+++ config/bundles.php
@@ -17,4 +17,5 @@ return [
     Nelmio\ApiDocBundle\NelmioApiDocBundle::class => ['all' => true],
     Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
     JMS\SerializerBundle\JMSSerializerBundle::class => ['all' => true],
+    Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
 ];
